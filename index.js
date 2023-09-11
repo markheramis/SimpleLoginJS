efd7227ce543a5c9e44402d6814dbf6c816a5e3b
Mark Heramis <chumheramis@gmail.com>
2023-09-11 21:06:36 +0800

    add multi-user and add registration function
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- index.js
+++ index.js
@@ -1,38 +1,72 @@
 const express = require('express');
 const session = require('express-session');
 const bodyparser = require('body-parser');
-
 var path = require('path');
-const ex = express();
-
-
-var username = 'admin';
-var password = 'admin';
-
-ex.use(session({secret:'sessionsecret777'}));
-ex.use(bodyparser.urlencoded({extended:true}));
-ex.engine('html', require('ejs').renderFile);
-ex.set('view engine','html');
-ex.use('/public', express.static(path.join(__dirname, 'public')))
-ex.set('views', path.join(__dirname, '/views'));
-
-ex.post('/',(req,res)=>{
-    if(req.body.username == username && req.body.password == password){
-        req.session.login = username;
-        res.render('success');
-    }else{
-        res.render('index');
+const app = express();
+// This is the user array
+var users = [
+    {
+        username: 'admin',
+        password: 'admin'
+    },
+    {
+        username: 'normal',
+        password: 'normal'
+    },
+    {
+        username: 'niko',
+        password: 'niko'
     }
+]
+app.use(session({secret:'sessionsecret777'}));
+app.use(bodyparser.urlencoded({extended:true}));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine','html');
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, '/views'));
+
+app.post('/login',(request, response) => {
+    // loop through all users to see if the one of the user from the users array matches with the request body
+    for(var i = 0; i < users.length; i++) {
+        let username = users[i].username;
+        let password = users[i].password;
+        if(request.body.username == username && request.body.password == password) {
+            request.session.login = username;
+            return response.render('login-success');
+        }
+    }
+    return response.render('login-failed');
 })
 
-ex.get('/',(req, res)=>{
+app.post('/register', (request, response) => {
+    // get the number of users before doing the add (or push)
+    let usersBeforeAdd = users.length;
+    /**
+     * Get username and password from the request body
+     */
+    let username = request.body.username;
+    let password = request.body.password;
+    // get the number of users after doing the add (or push)
+    let usersAfterAdd = users.push({
+        username: username,
+        password: password
+    })
+    // if number of users before add is less than the number of users after add
+    if(usersBeforeAdd < usersAfterAdd) {
+        // our push command succeeded
+        return response.render('registration-success');
+    } else {
+        // our push command failed
+        return response.render('registration-failed');
+    }
+});
+app.get('/',(req, res)=>{
     if(req.session.login){
         res.render('success');
     }else{
         res.render('index');
     }
 })
-
-ex.listen(8080,()=>{
-    console.log("Server tá on");
+app.listen(8080,()=>{
+    console.log("Server listening at port 8080");
 })
